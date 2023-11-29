cd2f94ab2203f10bc33af0b07c406c05e8218f29
lucascidade <lucascidade96@gmail.com>
2023-11-29 17:34:14 -0400

    implementação do CreateCommand e ICommand

---
+++ Todo.Domain/Commands/Contracts/ICommand.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Flunt.Notifications;
+using Flunt.Validations;
+namespace Todo.Domain.Commands.Contracts
+{
+    public interface ICommand
+    {
+        bool Validate();
+    }
+}
