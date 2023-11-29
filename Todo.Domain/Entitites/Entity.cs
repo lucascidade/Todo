60729f4f37ed839acecf0de39d37f4395efd0998
lucascidade <lucascidade96@gmail.com>
2023-11-29 16:17:26 -0400

    Implementação da class base Entity

--- Todo.Domain/Entitites/Entity.cs
+++ Todo.Domain/Entitites/Entity.cs
@@ -1,7 +1,10 @@
 namespace Todo.Domain.Entitites
 {
-    public abstract class Entity
+    public abstract class Entity : IEquatable<Entity>
     {
+        public Entity() => Id = Guid.NewGuid();
+        public Guid Id { get; private set; }
 
+        public bool Equals(Entity? other) => Id == other?.Id;
     }
 }
