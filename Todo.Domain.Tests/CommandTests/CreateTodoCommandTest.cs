0dfa292ced3b30b783a950a703b7ff6b8e042050
lucascidade <lucascidade96@gmail.com>
2023-12-05 12:47:36 -0400

    refatorando testes do CreateTodoCommand

--- Todo.Domain.Tests/CommandTests/CreateTodoCommandTest.cs
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTest.cs
@@ -5,22 +5,22 @@ namespace Todo.Domain.Tests.CommandTest;
 [TestClass]
 public class CreateTodoCommandTest
 {
+    private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+    private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Tarefa 1", "lucas cidade", DateTime.Now);
     [TestMethod]
     public void DadoUmComandoInvalido()
     {
-        var command = new CreateTodoCommand("", "", DateTime.Now);
-        command.Validate();
+        _invalidCommand.Validate();
         //comaparar o valid com o false, ou seja, garantir que o Validade seja FALSO // resultado esperado é TRUE F+F = V
-        Assert.AreEqual(command.Valid, false);
+        Assert.AreEqual(_invalidCommand.Valid, false);
     }
 
 
     [TestMethod]
     public void DadoUmComandoValido()
     {
-        var command = new CreateTodoCommand("Tarefa", "lucas cidade", DateTime.Now);
-        command.Validate();
-        Assert.AreEqual(command.Valid, true);
+        _validCommand.Validate();
+        Assert.AreEqual(_validCommand.Valid, true);
     }
 
 
